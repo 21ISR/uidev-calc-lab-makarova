7012180a4180c7e7e54304118f95a994e50566f7
Макарова <ovppov29@gmail.com>
2026-05-27 05:41:45 +0000

    134

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+
+let currentExpression = ''
+
+function handleButton(button) {
+    const value = button.target.textContent;
+}
+
+if (value === '='){
+    resultCal()
+} else if (value === 'AC'){
+    clearCal()
+} else if (value === '+/-'){
+    Toggle()
+} else if (value === '%'){
+    handle()
+} else {
+    currentExpression += value
+    update()
+}
+
+function update(){
+    display.textContent = currentExpression || '0'
+}
+
+function clearCal(){
+    
+}
